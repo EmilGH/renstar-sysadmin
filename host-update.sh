ff60c8865714d73117f1d8ffd9019f7458fbf751
Emil Rensing <emil@rensing.com>
2023-11-01 16:38:03 -0400

    Filename flip woes...

--- host-update.sh
+++ host-update.sh
@@ -2,17 +2,44 @@
 
 echo
 echo ================================================================================
-echo Renstar Global LLC - Generic Host Package Installation Utility
+echo Renstar Global LLC - Generic Host Update Utility *nix Systems v1
 echo ================================================================================
 echo
 
-echo Installing APT Packages... Please Wait.
+echo Updating APT Packages... Please Wait.
 echo
 
-/usr/bin/sudo /usr/bin/apt update && /usr/bin/sudo /usr/bin/apt install neofetch lolcat fortune ncdu mc
+/usr/bin/sudo /usr/bin/apt update && /usr/bin/sudo /usr/bin/apt upgrade -y && /usr/bin/sudo /usr/bin/apt autoremove
 
 echo
-echo APT Package Installation Complete.
+echo APT Package Update Complete.
+echo
+
+if [ -f "/usr/local/bin/pihole" ]
+then
+	echo Updating Pi-Hole...
+	echo
+
+	/usr/bin/sudo /usr/local/bin/pihole -up
+
+	echo
+	echo Pi-Hole Update Complete.
+	echo
+fi
+
+if [ -f "/usr/local/bin/hb-service" ]
+then
+	echo Updating Homebridge NODE...
+	echo
+
+	/usr/bin/sudo /usr/local/bin/hb-service update-node
+
+	echo
+	echo Homebridge Node Update Complete.
+	echo
+fi
+
+echo Host Update Complete.
 echo
 echo Have a _____ day.
 echo
