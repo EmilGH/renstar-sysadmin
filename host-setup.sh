884830fd03a584e86d627d462fee587ed695ac4c
Emil Rensing <emil@rensing.com>
2023-11-01 16:35:44 -0400

    First commit for host-setup.sh

---
+++ host-setup.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+echo
+echo ================================================================================
+echo Renstar Global LLC - Generic Host Update Utility *nix Systems v1
+echo ================================================================================
+echo
+
+echo Updating APT Packages... Please Wait.
+echo
+
+/usr/bin/sudo /usr/bin/apt update && /usr/bin/sudo /usr/bin/apt upgrade -y && /usr/bin/sudo /usr/bin/apt autoremove
+
+echo
+echo APT Package Update Complete.
+echo
+
+if [ -f "/usr/local/bin/pihole" ]
+then
+	echo Updating Pi-Hole...
+	echo
+
+	/usr/bin/sudo /usr/local/bin/pihole -up
+
+	echo
+	echo Pi-Hole Update Complete.
+	echo
+fi
+
+if [ -f "/usr/local/bin/hb-service" ]
+then
+	echo Updating Homebridge NODE...
+	echo
+
+	/usr/bin/sudo /usr/local/bin/hb-service update-node
+
+	echo
+	echo Homebridge Node Update Complete.
+	echo
+fi
+
+echo Host Update Complete.
+echo
+echo Have a _____ day.
+echo
