cb20e7f6e327261b62590417bc46b9153aafaddb
Emil Rensing <emil@rensing.com>
2023-11-01 16:31:45 -0400

    First commit for host-setup.sh

--- host-update.sh
+++ host-update.sh
@@ -2,44 +2,17 @@
 
 echo
 echo ================================================================================
-echo Renstar Global LLC - Generic Host Update Utility *nix Systems v1
+echo Renstar Global LLC - Generic Host Package Installation Utility
 echo ================================================================================
 echo
 
-echo Updating APT Packages... Please Wait.
+echo Installing APT Packages... Please Wait.
 echo
 
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove
+/usr/bin/sudo /usr/bin/apt update && /usr/bin/sudo /usr/bin/apt install neofetch lolcat fortune ncdu mc
 
 echo
-echo APT Package Update Complete.
-echo
-
-if [ -f "/usr/local/bin/pihole" ]
-then
-	echo Updating Pi-Hole...
-	echo
-
-	sudo /usr/local/bin/pihole -up
-
-	echo
-	echo Pi-Hole Update Complete.
-	echo
-fi
-
-if [ -f "/usr/local/bin/hb-service" ]
-then
-	echo Updating Homebridge NODE...
-	echo
-
-	sudo /usr/local/bin/hb-service update-node
-
-	echo
-	echo Homebridge Node Update Complete.
-	echo
-fi
-
-echo Host Update Complete.
+echo APT Package Installation Complete.
 echo
 echo Have a _____ day.
 echo
