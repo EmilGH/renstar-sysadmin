aa72fcc8d4cf4e78c79666c36d268535152226f5
Emil Rensing <emil@rensing.com>
2023-11-01 16:16:50 -0400

    Updated host-update.sh to include Pi-Hole and Homebridge Node

--- host-update.sh
+++ host-update.sh
@@ -6,4 +6,40 @@ echo Renstar Global LLC - Generic Host Update Utility *nix Systems v1
 echo ================================================================================
 echo
 
+echo Updating APT Packages... Please Wait.
+echo
+
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove
+
+echo
+echo APT Package Update Complete.
+echo
+
+if [ -f "/usr/local/bin/pihole" ]
+then
+	echo Updating Pi-Hole...
+	echo
+
+	sudo /usr/local/bin/pihole -up
+
+	echo
+	echo Pi-Hole Update Complete.
+	echo
+fi
+
+if [ -f "/usr/local/bin/hb-service" ]
+then
+	echo Updating Homebridge NODE...
+	echo
+
+	sudo /usr/local/bin/hb-service update-node
+
+	echo
+	echo Homebridge Node Update Complete.
+	echo
+fi
+
+echo Host Update Complete.
+echo
+echo Have a _____ day.
+echo
