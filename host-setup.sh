621bd4a4464165a4868239388be269d13b423d28
Emil Rensing <emil@rensing.com>
2023-11-14 14:19:58 -0500

    Added packages to setup script

--- host-setup.sh
+++ host-setup.sh
@@ -9,10 +9,21 @@ echo
 echo Installing APT Packages... Please Wait.
 echo
 
-/usr/bin/sudo /usr/bin/apt update && /usr/bin/sudo /usr/bin/apt install neofetch lolcat fortune ncdu mc
+/usr/bin/sudo /usr/bin/apt update && /usr/bin/sudo /usr/bin/apt install neofetch lolcat fortune ncdu mc cowsay
 
 echo
 echo APT Package Installation Complete.
 echo
+
+echo Installing Config Files... Please Wait.
+
+# .plan
+# .profile
+# .bashrc
+# .bash_logout
+# .bash_aliases
+# /etc/ profile
+# /etc/ bashrc
+
 echo Have a _____ day.
 echo
