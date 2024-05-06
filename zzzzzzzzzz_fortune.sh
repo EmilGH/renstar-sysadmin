d96165e38fd96698d4fc29daa20ba897b0bbdfdb
Emil Rensing <emil@rensing.com>
2024-05-06 07:21:55 -0400

    Create zzzzzzzzzz_fortune.sh
    Added global profile fortune script...

---
+++ zzzzzzzzzz_fortune.sh
@@ -0,0 +1,40 @@
+echo This is a private system owned and operated by Renstar Global LLC.
+echo
+echo Before processing any information, check the security accreditation level of this system
+echo to ensure proper clearance and accreditation. Do not process, store, or cause to be
+echo processed or stored or transmit information classified above the accreditation level of
+echo this system. This computer system including all related equipment, networks, and network
+echo devices are provided only for authorized use.
+echo
+echo Computer systems owned and operated by Renstar Global LLC and affiliates may be monitored
+echo for all lawful purposes, including to ensure that their use is authorized, for management
+echo of the system, to facilitate protection against unauthorized access, and to verify
+echo security procedures, survivability and operational security. Monitoring includes, but is
+echo not limited to, active attacks by authorized  entities to test or verify the security of
+echo the system. During monitoring, information may be examined, recorded, copied and used for
+echo authorized purposes. All information including personal information, placed on or sent
+echo over this system may be monitored.
+echo
+echo Use of this system, authorized or unauthorized, constitutes consent to monitoring.
+echo Unauthorized use of this computer system may subject you to criminal prosecution. Evidence
+echo of unauthorized use collected during monitoring may be used for administrative, criminal,
+echo or other adverse action. Use of this system constitutes consent to monitoring for all
+echo lawful purposes.
+echo
+echo This system comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law.
+echo
+echo Have a nice day.
+
+/usr/bin/hostname | /usr/bin/toilet --font mono9 --filter gay
+
+/usr/bin/ansiweather -u imperial -l "upper saddle river,us"
+
+echo
+
+/usr/bin/neofetch
+
+/usr/bin/inxi -D
+
+/usr/games/fortune -n 100 | /usr/games/cowsay | /usr/games/lolcat
+
+echo
